2678459addeee785007a29fc867323d9cdc78b95
root <root@lamp.(none)>
2013-08-01 06:53:41 +0000

    risolto problema riguardante messaggio di errore sul login

--- actions/login.php
+++ actions/login.php
@@ -6,8 +6,9 @@
  * @subpackage User.Authentication
  */
 
+ 
 // set forward url
-if (!empty($_SESSION['last_forward_from'])) {
+/*if (!empty($_SESSION['last_forward_from'])) {
 	$forward_url = $_SESSION['last_forward_from'];
 	unset($_SESSION['last_forward_from']);
 } elseif (get_input('returntoreferer')) {
@@ -15,7 +16,7 @@ if (!empty($_SESSION['last_forward_from'])) {
 } else {
 	// forward to main index page
 	$forward_url = '';
-}
+}*/
 
 $username = get_input('username');
 $password = get_input('password', null, false);
@@ -23,44 +24,50 @@ $persistent = (bool) get_input("persistent");
 $result = false;
 
 if (empty($username) || empty($password)) {
-	register_error(elgg_echo('login:empty'));
-	forward();
-}
-
-// check if logging in with email address
-if (strpos($username, '@') !== false && ($users = get_user_by_email($username))) {
-	$username = $users[0]->username;
-}
-
-$result = elgg_authenticate($username, $password);
-if ($result !== true) {
-	register_error($result);
-	forward(REFERER);
-}
-
-$user = get_user_by_username($username);
-if (!$user) {
-	register_error(elgg_echo('login:baduser'));
-	forward(REFERER);
-}
-
-try {
-	login($user, $persistent);
-	// re-register at least the core language file for users with language other than site default
-	register_translations(dirname(dirname(__FILE__)) . "/languages/");
-} catch (LoginException $e) {
-	register_error($e->getMessage());
-	forward(REFERER);
-}
-
-// elgg_echo() caches the language and does not provide a way to change the language.
-// @todo we need to use the config object to store this so that the current language
-// can be changed. Refs #4171
-if ($user->language) {
-	$message = elgg_echo('loginok', array(), $user->language);
+	//register_error(elgg_echo('login:empty'));
+	system_message(elgg_echo('login:empty'));
+	//forward();
 } else {
-	$message = elgg_echo('loginok');
-}
 
-system_message($message);
-forward($forward_url);
+		// check if logging in with email address
+		if (strpos($username, '@') !== false && ($users = get_user_by_email($username))) {
+			$username = $users[0]->username;
+		}
+
+		$result = elgg_authenticate($username, $password);
+		if ($result !== true) {
+			//register_error($result);
+			$message = $result;
+			system_message($message);
+			//forward(REFERER);
+			error_log("non riesco");
+		} else {
+			error_log("loggato");
+			$user = get_user_by_username($username);
+			if (!$user) {
+				//register_error(elgg_echo('login:baduser'));
+				//forward(REFERER);
+			}
+
+			try {
+				login($user, $persistent);
+				// re-register at least the core language file for users with language other than site default
+				register_translations(dirname(dirname(__FILE__)) . "/languages/");
+			} catch (LoginException $e) {
+				register_error($e->getMessage());
+				//forward(REFERER);
+			}
+
+			// elgg_echo() caches the language and does not provide a way to change the language.
+			// @todo we need to use the config object to store this so that the current language
+			// can be changed. Refs #4171
+			if ($user->language) {
+				$message = elgg_echo('loginok', array(), $user->language);
+			} else {
+				$message = elgg_echo('loginok');
+			}
+		}
+}
+//error_log($message);
+//system_message($message);
+//forward($forward_url);
