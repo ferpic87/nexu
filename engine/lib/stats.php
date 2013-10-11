68036d79364cda58ce1421db0614bb172ed651bf
root <root@lamp.(none)>
2013-10-11 07:06:03 +0000

    modifica al servizio statistiche

--- engine/lib/stats.php
+++ engine/lib/stats.php
@@ -2,8 +2,10 @@
 
 include_once("entities.php");
 
-function get_stats($type, $month = 0, $perTool = 0, $interactionType, $tool = 0)  {
+function get_stats($type, $timeStart,$timeEnd, $perTool = 0, $interactionType, $tool = 0)  {
 	$response = array();
+	
+	error_log("type ".$type." timeStart ".$timeStart." timeEnd ".$timeEnd."  perTool ".$perTool." interactionType ".$interactionType." tool ".$tool);
 	
 //	$month = $temp[0];
 //	$year = $temp[1];
@@ -19,7 +21,7 @@ function get_stats($type, $month = 0, $perTool = 0, $interactionType, $tool = 0)
 	
 		$count = count($subtypes);	
 		for($i=0; $i<$count; $i++) {
-			$arrResponse = doQuery($type, $month, $interactionType, array($subtypes[$i]), $aliases[$i]);
+			$arrResponse = doQuery($type, $timeStart,$timeEnd, $interactionType, array($subtypes[$i]), $aliases[$i]);
 			$temp = array("blog" => 0, "status_update" => 0, "bookmark" => 0, "file" => 0, "discussion" => 0);
 			foreach($arrResponse as $value) {
 				if(!isset($response[$value->guid])) {
@@ -30,7 +32,7 @@ function get_stats($type, $month = 0, $perTool = 0, $interactionType, $tool = 0)
 			}
 		}
 	} else {
-		$respQuery = doQuery($type, $month, $interactionType, $subtypes, "value");
+		$respQuery = doQuery($type, $timeStart,$timeEnd, $interactionType, $subtypes, "value");
 		$temp = array("name" => 0, "value" => 0);
 		foreach($respQuery as $item) {
 			$response[$item->guid] = $temp;
@@ -41,8 +43,7 @@ function get_stats($type, $month = 0, $perTool = 0, $interactionType, $tool = 0)
 	return $response;
 }
 
-function doQuery($type, $monthYear, $interactionType, $subtypes, $alias) {	
-	
+function doQuery($type, $timeStart,$timeEnd, $interactionType, $subtypes, $alias) {	
 	if($interactionType == "creator") {
 		if($type == "usage_frequency") {
 			$toSelect = "user.guid as guid, user.name as name";
@@ -51,7 +52,7 @@ function doQuery($type, $monthYear, $interactionType, $subtypes, $alias) {
 			$toSelect = "log.id as guid, object_subtype as name";
 			$groupby = "object_subtype";	
 		}
-		return getContentCreated($toSelect, $groupby, $type, $month, $interactionType, $subtypes, $alias);
+		return getContentCreated($toSelect, $groupby, $type, $timeStart,$timeEnd, $interactionType, $subtypes, $alias);
 	} else if($interactionType == "engaged") {
 		
 		if($type == "usage_frequency") {
@@ -61,13 +62,13 @@ function doQuery($type, $monthYear, $interactionType, $subtypes, $alias) {
 			$toSelect = "sub.id as guid, sub.subtype as name";
 			$groupby = "sub.subtype";	
 		}
-		return getNumberOfInteractions($toSelect, $groupby, $type, $month, $subtypes, $alias, "all");
+		return getNumberOfInteractions($toSelect, $groupby, $type, $timeStart,$timeEnd, $subtypes, $alias, "all");
 	}
 }
 
 
-function getContentCreated($fieldToSelect, $groupby, $type, $month, $interactionType, $subtypes, $alias) {
-	$monthClause = "true";
+function getContentCreated($fieldToSelect, $groupby, $type, $timeStart,$timeEnd, $interactionType, $subtypes, $alias) {
+	$timeClause = "true";
 
 	$subtypeClause = "(log.object_subtype = '".$subtypes[0]."')";
 	$subtypeCount = count($subtypes);
@@ -79,19 +80,25 @@ function getContentCreated($fieldToSelect, $groupby, $type, $month, $interaction
 	else if($interactionType == "engaged")
 		$eventTypeClause = "log.event = 'annotate'";
 
-	if($month != 0) {
-		$monthClause = "month(from_unixtime(log.time_created)) = '".$month."'"; 
+	if($timeStart!= 0 && $timeEnd !=0) {
+		//$timeClause = "log.time_created >= '".$timeStart."' AND log.time_created <='".$timeEnd."'"; 
+		$timeClause = "log.time_created BETWEEN '".$timeStart."' AND '".$timeEnd."'"; 
 	}
+	if($timeStart!= 0 && $timeEnd ==0) {
+		//$timeClause = "log.time_created >= '".$timeStart."' AND log.time_created <='".$timeEnd."'"; 
+		$timeClause = "log.time_created >='".$timeStart."'"; 
+	}
+
 	
-	$query = "select $fieldToSelect,count(*) as $alias from elgg_system_log log join elgg_users_entity user on (log.performed_by_guid = user.guid) join elgg_objects_entity obj on (log.object_id = obj.guid) where log.time_created >= 1372636800 and log.object_type = 'object' and ($subtypeClause) and user.name <> 'admin' and ($monthClause) and log.event = 'create' group by $groupby order by $alias desc";
+	$query = "select $fieldToSelect,count(*) as $alias from elgg_system_log log join elgg_users_entity user on (log.performed_by_guid = user.guid) join elgg_objects_entity obj on (log.object_id = obj.guid) where log.time_created >= 1372636800 and log.object_type = 'object' and ($subtypeClause) and user.name <> 'admin' and ($timeClause) and log.event = 'create' group by $groupby order by $alias desc";
 	
 	$response = get_data($query);
 	error_log("q=".$query);
-	return $response;
+	return $response; 
 }
 
-function getNumberOfInteractions($fieldToSelect, $groupby, $type, $month, $subtypes, $alias, $what) {
-	$monthClause = "true";
+function getNumberOfInteractions($fieldToSelect, $groupby, $type, $timeStart,$timeEnd, $subtypes, $alias, $what) {
+	$timeClause = "true";
 	
 	$subtypeClause = "(sub.subtype = '".$subtypes[0]."')";
 	$subtypeCount = count($subtypes);
@@ -105,12 +112,19 @@ function getNumberOfInteractions($fieldToSelect, $groupby, $type, $month, $subty
 		$interactionClause = "name_id = 443"; 
 	}
 	
-	if($month != 0) {
-		$monthClause = "month(from_unixtime(ann.time_created)) = '".$month."'"; 
+	if($timeStart!= 0 && $timeEnd !=0) {
+		//$timeClause = "month(from_unixtime(ann.time_created)) = '".$month."'";
+		//$timeClause = "log.time_created >= '".$timeStart."' AND log.time_created <='".$timeEnd."'"; 
+		$timeClause = "ann.time_created BETWEEN '".$timeStart."' AND '".$timeEnd."'"; 
 	}
+		if($timeStart!= 0 && $timeEnd ==0) {
+		//$timeClause = "log.time_created >= '".$timeStart."' AND log.time_created <='".$timeEnd."'"; 
+		$timeClause = "ann.time_created >='".$timeStart."'"; 
+	}
+
 	
-	$query = "SELECT $fieldToSelect, count(*) as $alias FROM elgg_annotations ann JOIN elgg_users_entity user ON ( ann.owner_guid = user.guid ) JOIN elgg_entities ent ON ( ann.entity_guid = ent.guid ) JOIN elgg_entity_subtypes sub ON ( ent.subtype = sub.id) WHERE  ann.time_created >= 1372636800 and ($interactionClause) and ($subtypeClause) and $monthClause group by $groupby order by $alias desc";
+	$query = "SELECT $fieldToSelect, count(*) as $alias FROM elgg_annotations ann JOIN elgg_users_entity user ON ( ann.owner_guid = user.guid ) JOIN elgg_entities ent ON ( ann.entity_guid = ent.guid ) JOIN elgg_entity_subtypes sub ON ( ent.subtype = sub.id) WHERE  ann.time_created >= 1372636800 and ($interactionClause) and ($subtypeClause) and $timeClause group by $groupby order by $alias desc";
 	$response = get_data($query);
-	//error_log("q=".$query);
+	error_log("q=".$query);
 	return $response;
 }
